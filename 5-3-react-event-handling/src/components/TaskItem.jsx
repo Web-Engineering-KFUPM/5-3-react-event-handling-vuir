05b3244fad9dfbd73cccd9a2d91db511ec2e711b
Vuir <Taevio@hotmail.com>
2025-10-06 12:07:13 +0300

    Task 3

--- 5-3-react-event-handling/src/components/TaskItem.jsx
+++ 5-3-react-event-handling/src/components/TaskItem.jsx
@@ -12,7 +12,7 @@ export default function TaskItem({ id, text, onDelete }) {
           className="iconBtn iconBtn--danger"
           aria-label="Delete task"
           title="Delete"
-          // TODO: onClick={() => onDelete(id)}
+          onClick={() => onDelete(id)}
         >
           🗑️
         </button>
